69b5f3e791a3f217d18727671b50ab98813dab62
Deepa Francis <89530570+DeepaFrancis@users.noreply.github.com>
2022-02-02 21:22:44 -0600

    update

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,8 +1,5 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
-
-
-
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
@@ -17,7 +14,18 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
         maxZoom: 18,
         accessToken: API_KEY
     });
-    
+ //  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Then we add our 'graymap' tile layer to the map.
+//L.circle([34.0522, -118.2437], {
+  //radius: 100
+//}).addTo(map);
+L.circleMarker([34.0522, -118.2437],{
+  raduis:300,
+  color:"black",
+  fillColor: '#ffffa1'
+}).addTo(map);
+streets.addTo(map);
     // Then we add our 'graymap' tile layer to the map.
     streets.addTo(map);
 
