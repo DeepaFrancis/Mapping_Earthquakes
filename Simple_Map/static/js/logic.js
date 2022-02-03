ac93baeb7e336fad298edfc6f1d043a85d5ff09d
Deepa Francis <89530570+DeepaFrancis@users.noreply.github.com>
2022-02-02 20:15:53 -0600

    update

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,8 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
